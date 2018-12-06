ca930c9b8167da0bf1c1f6e93d11266d0ed97e14
AntonAdigamov <AdigamovAntonAAA@gmail.com>
2018-12-06 18:11:41 +0100

    Finish chapter 7

--- palindrome.js
+++ palindrome.js
@@ -1,10 +1,23 @@
-// Reverses a string.
-function reverse(string) {
-  return string.split("").reverse().join("");
+// Adds `reverse` to all strings.
+String.prototype.reverse = function() {
+  return Array.from(this).reverse().join("");
 }
 
-// Returns true for a palindrome, false otherwise.
-function palindrome(string) {
-  let processedContent = string.toLowerCase();
-  return processedContent === reverse(processedContent);
+// Defines a Phrase object.
+function Phrase(content) {
+  this.content = content;
+
+  this.processor = function(string) {
+    return string.toLowerCase();
+  }
+
+  // Returns content processed for palindrome testing.
+  this.processedContent = function processedContent() {
+    return this.processor(this.content);
+  }
+
+  // Returns true for a palindrome, false otherwise.
+  this.palindrome = function palindrome() {
+    return this.processedContent() === this.processedContent().reverse();
+  }
 }
